bdabd0abc4fa3a00d37657b320ca2829efb5b384
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-28 16:41:48 +0530

    Create error handling middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import pool from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,13 +11,16 @@ const port = process.env.PORT || 3001;
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
 
 // routes
 
 app.get("/", async(req , res)=>{
     const result = await pool.query("SELECT current_database()");
     res.json(`The database name is : ${result.rows[0].current_database}`);
-})
+});
+
+app.use("/api", userRoutes);
 
 
 app.listen(port, () => {
