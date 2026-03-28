dba96b4fb24ee516e460d9c5371ba94364456592
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-28 13:48:20 +0530

    configure db connection

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import pkg from "pg";
+import "dotenv/config";
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT
+});
+
+pool.on("connect", () => {
+    console.log("Connection pool established");
+});
+
+export default pool;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import pool from "./config/db";
 
 const app = express();
 const port = process.env.PORT || 3001;
