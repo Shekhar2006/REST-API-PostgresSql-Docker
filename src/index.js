9d578a35779790511e7cdf838b2c00eda6e56cb2
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-28 12:47:21 +0530

    Setup server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
